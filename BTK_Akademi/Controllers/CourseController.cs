d39db5d9268d9152124766a0a73bb393686061b1
muffafa <galipsamilduger@gmail.com>
2024-07-31 16:51:59 +0300

    Add project files.

---
+++ BTK_Akademi/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BtkAkademi.Entities;
+
+namespace BtkAkademi.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Apply(Candidate model)
+        {
+            return View();
+        }
+    }
+}
---
+++ BtkAkademi.Entities/Candidate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtkAkademi.Entities
+{
+
+    public class Candidate
+    {
+        [EmailAddress]
+        public String? Email { get; set; } = String.Empty;
+
+        public String? FirstName { get; set; } =string.Empty;
+
+        public String? LastName { get; set;} =string.Empty;
+
+        public string? FullName => $"{FirstName} {LastName?.ToUpper()}";
+
+        public int? Age { get; set; }
+
+        public String? SelectedCourse { get; set; } = String.Empty;
+
+        public DateTime ApplyAt {  get; set; }
+
+        public DateTime Apply { get; set; }
+        
+
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+
+
+
+
+
+
+    }
+}
